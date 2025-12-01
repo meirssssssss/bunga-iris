2dfd4a126b73662f4d8f50dc2e7ac34f56bfdbaf
meirssssssss <mariomeirss@gmail.com>
2025-12-01 15:37:09 +0700

    Update markdown formatting for better readability

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import numpy as np
 from prediction import predict
 
 st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play to classify iris flowers into
-(setosa, versicolor, virginica) based on their sepal/petal
-and lengthwidth.')
+st.markdown('Toy model to play to classify iris flowers into \
+    (setosa, versicolor, virginica) based on their sepal/petal \
+    and lengthwidth.')
 
 st.header("Plant Features")
 col1, col2 = st.columns(2)
